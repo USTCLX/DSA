7146c6ebbee6026b03be8f750c5d56f98fb27fd9
Xiang LI <xlil@ctrip.com>
2020-05-24 23:21:01 +0800

    update - leetcode 15

---
+++ src/leetcode/15/index.test.ts
@@ -0,0 +1,13 @@
+import { threeNumSum } from "./index";
+
+describe("threeNumSum", () => {
+  test("[-1, 0, 1, 2, -1, -4] ", () => {
+    const res = threeNumSum([-1, 0, 1, 2, -1, -4]);
+    expect(res).toMatch(
+      expect.arrayContaining([
+        [-1, 0, 1],
+        [-1, -1, 2]
+      ])
+    );
+  });
+});
