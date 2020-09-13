7369b372aa1a577e730d45501dd68abd457f249e
Xiang LI <xlil@ctrip.com>
2020-09-13 12:42:36 +0800

    update - quicksort

---
+++ src/algorithm/sort/quickSort/index.test.ts
@@ -0,0 +1,21 @@
+import quickSort from "./index";
+
+describe("mergeSort", () => {
+  test("[]", () => {
+    expect(quickSort([])).toEqual([]);
+  });
+  test("[1]", () => {
+    expect(quickSort([1])).toEqual([1]);
+  });
+
+  test("[1,2]", () => {
+    expect(quickSort([1, 2])).toEqual([1, 2]);
+  });
+
+  test("[3,2,1]", () => {
+    expect(quickSort([3, 2, 1])).toEqual([1, 2, 3]);
+  });
+  test("[2,1,4,5,3,1]", () => {
+    expect(quickSort([2, 1, 4, 5, 3, 1])).toEqual([1, 1, 2, 3, 4, 5]);
+  });
+});
