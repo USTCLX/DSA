c39333a52582ddc7f989b73345439cbbdc6452c8
Xiang Li <xlil@trip.com>
2020-11-19 19:24:32 +0800

    udpate

--- src/leetcode/139.单词拆分.js
+++ src/leetcode/139.单词拆分.js
@@ -17,30 +17,52 @@
 // 2. 第二种思路更加的合理清晰
 //    从0开始往期，尽量找出一个单词在set中，如果在了，就再去尝试找后面剩下的字符串，知道最后没有剩下字符串，就说明找到了一个这样的组合。
 // 3. 带来的启发就是，不要试图简单暴力的求出所有组合，还是要将问题分解和找规律。将问题分解为更小的问题，然后看看小问题和原问题是否是类似的
-var canBreak = function(str, words, memo) {
-  if (!str.length) {
-    // 如果是一个空字符，默认是可以在word中找到（因为也不需要找）
-    return true;
-  }
+// var canBreak = function(str, words, memo) {
+//   if (!str.length) {
+//     // 如果是一个空字符，默认是可以在word中找到（因为也不需要找）
+//     return true;
+//   }
 
-  if (memo.has(str)) {
-    return false;
-  }
-  for (let i = 0; i < str.length; i++) {
-    if (
-      words.has(str.slice(0, i + 1)) &&
-      canBreak(str.slice(i + 1), words, memo)
-    ) {
+//   if (memo.has(str)) {
+//     return false;
+//   }
+//   for (let i = 0; i < str.length; i++) {
+//     if (
+//       words.has(str.slice(0, i + 1)) &&
+//       canBreak(str.slice(i + 1), words, memo)
+//     ) {
+//       return true;
+//     }
+//   }
+//   memo.add(str);
+//   return false;
+// };
+// var wordBreak = function(str, wordDict) {
+//   const words = new Set(wordDict);
+//   const memo = new Set();
+//   return canBreak(str, words, memo);
+// };
+
+var canBreak = function(str, wordDict, memo) {
+  if (!str) return true;
+  if (memo.has(str)) return false;
+  const len = str.length;
+  for (let i = 0; i < len; i++) {
+    const s1 = str.slice(0, i + 1);
+    const s2 = str.slice(i + 1);
+    if (wordDict.has(s1) && canBreak(s2, wordDict, memo)) {
       return true;
     }
   }
   memo.add(str);
   return false;
 };
+
 var wordBreak = function(str, wordDict) {
-  const words = new Set(wordDict);
+  const dict = new Set(wordDict);
   const memo = new Set();
-  return canBreak(str, words, memo);
+
+  return canBreak(str, dict, memo);
 };
 
 // @lc code=end
