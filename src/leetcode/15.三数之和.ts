28584f8f84d47315d6c51c55d67ea732645e862f
LX-PC-Win10 <herrycodelee@gmail.com>
2021-01-19 09:38:23 +0800

    update - 再做三数之和

--- src/leetcode/15.三数之和.ts
+++ src/leetcode/15.三数之和.ts
@@ -4,34 +4,82 @@
  * [15] 三数之和
  */
 
+// function threeSum(nums: number[]): number[][] {
+//   nums.sort((a, b) => a - b);
+
+//   const result = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] > 0) break;
+
+//     if (nums[i] === nums[i - 1]) continue;
+
+//     let left = i + 1;
+//     let right = nums.length - 1;
+//     while (left < right) {
+//       const sum = nums[i] + nums[left] + nums[right];
+//       if (sum === 0) {
+//         result.push([nums[i], nums[left], nums[right]]);
+//         while (nums[left] === nums[left + 1]) left++;
+//         while (nums[right] === nums[right - 1]) right--;
+//         left++;
+//         right--;
+//       } else if (sum < 0) {
+//         left++;
+//       } else if (sum > 0) {
+//         right--;
+//       }
+//     }
+//   }
+//   return result;
+// }
+
 // @lc code=start
 
+// 三数之和，不建议使用map的方法来做了，map方法比较适合两数之和
+// 因为还需要去重，使用先排序，在夹逼的方式来做
 function threeSum(nums: number[]): number[][] {
-  nums.sort((a, b) => a - b);
+  // 排序数组
+  nums = nums.sort((a, b) => a - b);
 
-  const result = [];
+  const res = [];
+
+  // 遍历
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > 0) break;
-
-    if (nums[i] === nums[i - 1]) continue;
-
+    const target = nums[i];
     let left = i + 1;
     let right = nums.length - 1;
+
+    // 如果目标大于0，后面的数也都大于0，肯定找不到了。优化
+    if (target > 0) break;
+
+    // 如果target和上一个target一样，跳过
+    if (target === nums[i - 1]) continue;
+
+    // 夹逼寻找
     while (left < right) {
-      const sum = nums[i] + nums[left] + nums[right];
+      // 先算好sum，避免在判断中重复计算
+      const sum = target + nums[left] + nums[right];
       if (sum === 0) {
-        result.push([nums[i], nums[left], nums[right]]);
+        // 找到
+        res.push([target, nums[left], nums[right]]);
+
         while (nums[left] === nums[left + 1]) left++;
         while (nums[right] === nums[right - 1]) right--;
+
         left++;
         right--;
       } else if (sum < 0) {
+        // 偏小了，left右移动
         left++;
-      } else if (sum > 0) {
+      } else {
+        // 偏大了，right左移动
         right--;
       }
     }
   }
-  return result;
+
+  return res;
 }
 // @lc code=end
+
+export { threeSum };
