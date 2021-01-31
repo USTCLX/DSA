50cbae7056a93b1203311ca30a0d84e242ff439a
Xiang LI <xlil@ctrip.com>
2021-01-31 12:00:01 +0800

    update - 139 单词拆分

---
+++ src/leetcode/139.test.ts
@@ -0,0 +1,5 @@
+import { wordBreak } from "./139.单词拆分.ts";
+
+test("leetcode", () => {
+  expect(wordBreak("leetcode", ["leet", "code"])).toBe(true);
+});
--- src/leetcode/139.单词拆分.ts
+++ src/leetcode/139.单词拆分.ts
@@ -5,29 +5,51 @@
  */
 
 // @lc code=start
+// 递归的解法
+function check(
+  s: string,
+  wordDict: string[],
+  cache: Set<string> = new Set()
+): boolean {
+  if (!s) return true;
 
-function canBreak(str: string, words: Set<string>, memo: Set<string>) {
-  if (!str.length) return true;
+  if (cache.has(s)) {
+    return false;
+  }
 
-  if (memo.has(str)) return false;
-
-  for (let i = 0; i < str.length; i++) {
-    if (
-      words.has(str.slice(0, i + 1)) &&
-      canBreak(str.slice(i + 1), words, memo)
-    ) {
+  for (let i = 0; i < s.length; i++) {
+    const word = s.slice(0, i + 1);
+    if (wordDict.includes(word) && check(s.slice(i + 1), wordDict, cache)) {
       return true;
     }
   }
 
-  memo.add(str);
+  cache.add(s);
 
   return false;
 }
 
+// 动态规划的解法
+// dp[i] 代表0-i这个子串是否可以被拆分
+// dp[i] = dp[q] && words.include(s[q+1~i]); 其中q的范围是0～i-1
+function dp(s: string, wordDict: string[]): boolean {
+  const dp = Array(s.length + 1).fill(false);
+  dp[0] = true; // 默认 '' 在wordDict中
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j <= i; j++) {
+      if (dp[j] && wordDict.includes(s.slice(j, i + 1))) {
+        dp[i + 1] = true;
+        break;
+      }
+    }
+  }
+
+  return dp[dp.length - 1];
+}
+
 function wordBreak(s: string, wordDict: string[]): boolean {
-  const words = new Set(wordDict);
-  const memo = new Set<string>();
-  return canBreak(s, words, memo);
+  return dp(s, wordDict);
 }
 // @lc code=end
+
+export { wordBreak };
